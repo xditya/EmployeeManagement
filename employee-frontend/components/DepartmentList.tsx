2a830e060c39c850bdb57be6f31e5db685d387fd
Ferwin Lopez <ferwinlopezt@gmail.com>
2024-05-14 05:54:36 +0530

    feat : fix employeeList

--- employee-frontend/components/DepartmentList.tsx
+++ employee-frontend/components/DepartmentList.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+'use client'; // I assume this is some specific directive or comment for your environment
+import { getDepartments } from '@/lib/data';
+import { DepartmentSchema } from '@/lib/types';
+import React, { useEffect, useState } from 'react';
 
 const DepartmentList = () => {
-  return (
-    <div></div>
-  )
-}
+  const [departments, setDepartments] = useState<DepartmentSchema[]>([]);
 
-export default DepartmentList
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+ 
+        const fetchedDepartments = await getDepartments();
+ 
+        setDepartments(fetchedDepartments);
+      } catch (error) {
+        console.error('Error fetching departments:', error);
+      }
+    };
+
+    fetchDepartments();
+  }, [])
+
+  return (
+    <div>
+      <h2>List of Departments</h2>
+      <ul>
+        {departments.map((department,index) => (
+          <li key={index}>{department.departmentName}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DepartmentList;
